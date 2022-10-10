07d58be0a455012317fc37415cc21a19343b003a
WilberthColli <manuelcolli05@gmail.com>
2022-10-09 19:32:04 -0500

    Se han configurado los repositorios de spring Data Jpa a la aplicacion de empleos

---
+++ empleos/src/main/java/net/itinajero/service/db/CategoriasServiceJpa.java
@@ -0,0 +1,28 @@
+package net.itinajero.service.db;
+
+import java.util.List;
+
+import net.itinajero.model.Categoria;
+import net.itinajero.service.ICategoriasService;
+
+public class CategoriasServiceJpa implements ICategoriasService {
+
+	@Override
+	public void guardar(Categoria categoria) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Categoria> buscarTodas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Categoria buscarPorId(Integer idCategoria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
