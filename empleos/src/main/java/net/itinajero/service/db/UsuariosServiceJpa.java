60c41545ccf309f9899bdd6714bac32693770079
WilberthColli <manuelcolli05@gmail.com>
2022-10-14 01:25:07 -0500

    Se añadieron los metodos para recuperarlos roles de usuario en cada inicio d sesión, asi como la reenderización de atrubitos con anotaciones thymeleaf

--- empleos/src/main/java/net/itinajero/service/db/UsuariosServiceJpa.java
+++ empleos/src/main/java/net/itinajero/service/db/UsuariosServiceJpa.java
@@ -38,4 +38,10 @@ public class UsuariosServiceJpa implements IUsuariosService {
 		return usuariosRepo.findAll();
 	}
 
+	@Override
+	public Usuario buscarPorUsername(String username) {
+		// TODO Auto-generated method stub
+		return usuariosRepo.findByUsername(username);
+	}
+
 }
